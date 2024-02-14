b2fe2fee8320c6965663c35d9fd04ff2ee29bd98
mclaramarinho <marinho.claramb@gmail.com>
2024-02-14 15:09:15 -0300

    component: MainMenu

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-container class="position-absolute top-50 start-50 translate-middle w-fit">
+        <v-row v-for="button in buttons" :key="button" no-gutters class="mb-3">
+            <button-outline @click="(e : string)=>navigateSomewhere(e)" :text="button" />
+        </v-row>
+    </v-container>
+</template>
+
+<script lang="ts">
+import ButtonOutline from '../components/smaller_components/ButtonOutline.vue'
+    export default {
+        name: 'MainMenu',
+        components: {
+            ButtonOutline
+        },
+        data(){
+            return{
+                buttons: [
+                    "Horário de Aulas",
+                    "Calendário",
+                    "Contatos Importantes",
+                    "FAQ",
+                    "Admin"
+                ]
+            }
+        },
+        methods: {
+            navigateSomewhere(value : String){
+                this.$router.push('/' + value)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+
+.v-container{
+    padding: 0;
+}
+
+</style>
