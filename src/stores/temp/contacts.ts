f7a66df27c99fad6a33899c22a6e598819a8e40d
mclaramarinho <marinho.claramb@gmail.com>
2024-02-14 15:12:33 -0300

    data: contacts array

---
+++ src/stores/temp/contacts.ts
@@ -0,0 +1,42 @@
+export const contacts = [
+    {
+        name: 'Direção',
+        phone: '123456789',
+        email: 'mockupemail@email.com'
+    },
+    {
+        name: 'Secretaria',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    },
+    {
+        name: 'Secretaria',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    },
+    {
+        name: 'Direção',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    },
+    {
+        name: 'Secretaria',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    },
+    {
+        name: 'Direção',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    },
+    {
+        name: 'Secretaria',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    },
+    {
+        name: 'Direção',
+        phone: '123456789',
+        email: 'secretaria@gmail.com'
+    }
+]
