2948bf4445c7c56e340e5cd1b89c4df2396c7e9a
mclaramarinho <marinho.claramb@gmail.com>
2024-02-14 15:06:41 -0300

    component: App

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <v-app full-height>
+      <RouterView />
+  </v-app>
+</template>
+
+<style scoped>
+
+</style>
