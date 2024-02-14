0d1c4ee187c13eec5dc5abd5ba5595b0dd2ed916
mclaramarinho <marinho.claramb@gmail.com>
2024-02-14 15:10:34 -0300

    component: NavBar

---
+++ src/components/smaller_components/NavBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app-bar color="var(--dark-blue)" position-sticky>
+        <template v-slot:prepend>
+            <img src="../../assets/images/unit-logo.webp" alt="">
+        </template>
+    </v-app-bar>
+</template>
+
+<script lang="ts">
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<style scoped>
+    .v-app-bar {
+        position: sticky !important;
+    }
+</style>
