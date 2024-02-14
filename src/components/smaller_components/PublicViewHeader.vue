11568ea1b9e5f4547d24f45c5179c9a3f116782a
mclaramarinho <marinho.claramb@gmail.com>
2024-02-14 15:10:53 -0300

    component: PublicViewHeader

---
+++ src/components/smaller_components/PublicViewHeader.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-container>
+        <v-row no-gutters>
+            <v-col cols="12">
+                <h1 class="text-center font-blue">{{ title.toUpperCase() }}</h1>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+
+
+<script lang="ts">
+export default {
+    name: 'PublicViewHeader',
+    props: {
+        title: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped></style>
