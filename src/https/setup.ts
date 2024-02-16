7f52f2e9d0eead001307b52330ae5c08247d8f51
mclaramarinho <marinho.claramb@gmail.com>
2024-02-16 12:16:08 -0300

    config: Add HTTPS setup file

---
+++ src/https/setup.ts
@@ -0,0 +1,6 @@
+export const BASE_URL = 'https://unit-turminha-api.vercel.app/api';
+
+export const headers = {
+    'Content-Type': 'application/json',
+    'x-api-key': import.meta.env.VITE_API_KEY,
+}
