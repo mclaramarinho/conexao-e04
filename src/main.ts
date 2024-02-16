dc40eb63ca7b0bead70da3a58796d3ac55457c45
mclaramarinho <marinho.claramb@gmail.com>
2024-02-16 12:10:13 -0300

    config: Add VCalendar and Pinia to main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,27 @@ import 'vuetify/styles'
 import './assets/main.css'
 import { aliases, mdi } from 'vuetify/iconsets/mdi'
 import '@mdi/font/css/materialdesignicons.css'
+import { VCalendar } from 'vuetify/labs/VCalendar'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 
 const app = createApp(App)
 const vuetify = createVuetify({
-    icons: {
-        defaultSet: 'mdi',
-        aliases,
-        sets: {
-            mdi
+        icons: {
+            defaultSet: 'mdi',
+            aliases,
+            sets: {
+                mdi
+            },
         },
-    },
-    ssr: true,
+        components: {
+            VCalendar,
+        },
+        ssr: true,
     }
 );
 app.use(vuetify)
 app.use(router)
+app.use(pinia)
 app.mount('#app')
