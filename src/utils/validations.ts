37d8b67a437b2b117c28c6028b1b34913fe77cd9
mclaramarinho <marinho.claramb@gmail.com>
2024-02-16 12:17:33 -0300

    resource: Add validation functions for password, email, and notEmpty fields

---
+++ src/utils/validations.ts
@@ -0,0 +1,19 @@
+
+export const password = [
+    (v: string) => !!v || 'Senha é obrigatória',
+    (v: string) => v.length >= 8 || 'Senha deve ter no mínimo 8 caracteres',
+    (v: string) => v.length <= 20 || 'Senha deve ter no máximo 20 caracteres',
+    (v: string) => /[A-Z]/.test(v) || 'Senha deve ter pelo menos uma letra maiúscula',
+    (v: string) => /[a-z]/.test(v) || 'Senha deve ter pelo menos uma letra minúscula',
+    (v: string) => /[0-9]/.test(v) || 'Senha deve ter pelo menos um número',
+    (v: string) => /[!@#$%^&*()_+]/.test(v) || 'Senha deve ter pelo menos um caractere especial',
+];
+
+export const email = [
+    (v: string) => !!v || 'Email é obrigatório',
+    (v: string) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/.test(v) || 'Email deve ser válido',
+];
+
+export const notEmpty = [
+    (v : string) => !!v || 'Campo é obrigatório',
+];
