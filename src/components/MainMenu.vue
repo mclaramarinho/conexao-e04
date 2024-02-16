c9db939cf0d322e96a2ae06178476258a6e492f1
mclaramarinho <marinho.claramb@gmail.com>
2024-02-16 12:13:36 -0300

    Refactor: MainMenu.vue to use updated ButtonOutline component

--- src/components/MainMenu.vue
+++ src/components/MainMenu.vue
@@ -1,13 +1,14 @@
 <template>
     <v-container class="position-absolute top-50 start-50 translate-middle w-fit">
         <v-row v-for="button in buttons" :key="button" no-gutters class="mb-3">
-            <button-outline @click="(e : string)=>navigateSomewhere(e)" :text="button" />
+            <button-outline @click="(e : string)=>navigateSomewhere(e)" :text="button" v-if="button!=='Admin'" />
+            <button-outline @click="(e : string)=>navigateSomewhere(e)" value="admin/login" variant="tonal" :text="button" v-if="button==='Admin'" />
         </v-row>
     </v-container>
 </template>
 
 <script lang="ts">
-import ButtonOutline from '../components/smaller_components/ButtonOutline.vue'
+import ButtonOutline from '../components/smaller_components/buttons/ButtonOutline.vue'
     export default {
         name: 'MainMenu',
         components: {
