591cdc341e8bcb5a78409b19a63c3598b7c9e72a
mclaramarinho <marinho.claramb@gmail.com>
2024-02-16 12:14:45 -0300

    component: create

---
+++ src/components/smaller_components/buttons/TextBtn.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-btn 
+        :size="size"
+        :text="text"
+        variant="text"
+        :color="color"
+        :class="'mx-auto w-fit' + classes"
+        @click="() => $emit('clicked', value)" />
+</template>
+
+<script lang="ts">
+export default {
+    name: 'text-btn',
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        size:{
+            type: String,
+            default: "large",
+            required: false
+        },
+        color:{
+            type: String,
+            default: "var(--dark-blue)",
+            required: false
+        },
+        classes:{
+            type: String,
+            default: "",
+            required: false
+        },
+        value:{
+            type: String,
+            default: "def",
+            required: false
+        }
+    }
+}
+</script>
