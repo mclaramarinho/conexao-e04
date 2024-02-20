97b91ea493d02f7343268dbfc9cd603ecdc72e9e
mclaramarinho <marinho.claramb@gmail.com>
2024-02-19 21:07:31 -0300

    pinia: completed userInfo storage

--- src/stores/userInfo.ts
+++ src/stores/userInfo.ts
@@ -1,15 +1,41 @@
+import { isLoggedIn } from "@/firebase/authorization";
+import { admin_get_all, admin_get_one } from "@/https/admin";
+import type { User } from "firebase/auth";
 import { defineStore } from "pinia";
 
 export const useUserInfoStore = defineStore({
-    UID: null as  null | string,
-    name: null as string | null,
-    email: null as string | null,
-    displayName: null as string | null,
-    role: null as "owner" | "admin" | null,
-
+    id: "userInfo",
+    state: () => ({
+        UID: '' as String || null,
+        name: '' as String || null,
+        email: '' as String || null,
+        displayName: '' as String || null,
+        role: '' as String || null,
+    }),
     actions: {
-        update(){
-            
+        async update() : Promise<void>{
+            isLoggedIn().then((user) => {
+                if(user){
+                    const u = user as User;
+                    this.UID = u.uid;
+                    this.name = u.displayName as string;
+                    this.email = u.email as string;
+                    this.displayName = u.displayName as string;
+                    admin_get_one().then((res) => {
+                        this.role = res.role as string;
+                    }).catch(e => {
+                        console.log(e);
+                        throw new Error(e.message);
+                    })
+                }
+            }).catch(e => {
+                console.log(e);
+                this.UID = '';
+                this.name = '';
+                this.email = '';
+                this.displayName = '';
+                this.role = '';
+            })
         }
     }
 })
