397c84fcfcae7ebed80f6e991c036f5b4a5acac8
mclaramarinho <marinho.claramb@gmail.com>
2024-02-19 21:08:40 -0300

    Refactor: AdminDashboard.vue component

--- src/views/private/AdminDashboard.vue
+++ src/views/private/AdminDashboard.vue
@@ -1,17 +1,39 @@
 <template>
-    <v-container fluid class="h-100">
-        <v-navigation-drawer>
-            <v-list-item title=""></v-list-item>
-        </v-navigation-drawer>
+    <v-container fluid class="h-100 p-0">
+        
+        <v-row no-gutters class="h-100 w-100 m-0">
+                <nav-drawer 
+                    :email="(admin.email as string)" :name="(admin.name as string)" :role="(admin.role as string)" />
+        </v-row>
     </v-container>
 </template>
 
 <script lang="ts">
-import {admin_get_all, admin_get_one} from "../../https/admin"
+import type { IUser } from "@/interfaces/IUser";
+import {admin_get_one} from "../../https/admin"
+import NavDrawer from "@/components/smaller_components/NavDrawer.vue";
+import { useUserInfoStore } from "@/stores/userInfo";
+import { useNavigationHistory } from "@/stores/useNavigationHistory";
 export default {
     name: 'AdminDashboard',
+    components: {
+        NavDrawer
+    },
+    data() {
+        return {
+            admin: {} as IUser
+        }
+    },
+    beforeRouteUpdate(to, from, next) {
+        useNavigationHistory().setPreviousRoute(this.$route.path);
+        next()
+    },
+    beforeRouteLeave(to, from, next) {
+        useNavigationHistory().setPreviousRoute(from.path);
+        next()
+    },
     created() {
-        admin_get_one().then(r => console.log(r)).catch(e => console.log(e))
+        admin_get_one().then(r => this.admin = r).catch(e => this.admin = {} as IUser)
     }
 }
 </script>
