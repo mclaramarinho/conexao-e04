cbaa02d8ecddba3fe3d97d2fc823e569a7508ed3
mclaramarinho <marinho.claramb@gmail.com>
2024-02-20 21:52:28 -0300

    component: created event preview card

---
+++ src/components/smaller_components/cards/EventPreviewCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-card variant="tonal" class="font-blue p-3">
+        <v-card-title style="font-size: 2rem;">Nome do evento</v-card-title>
+        <v-card-subtitle class="mb-5" style="font-size: 1.2rem;">Obrigatório</v-card-subtitle>
+        <v-card-text>
+            <v-row no-gutters style="font-size: 1.2rem;">
+                <v-col cols="6">
+                    <v-row class="mb-2 bold" no-gutters>Local:</v-row>
+                    <v-row no-gutters>Biblioteca</v-row>
+                </v-col>
+                <v-col cols="6">
+                    <v-row class="mb-2 bold" no-gutters>Organizador:</v-row>
+                    <v-row no-gutters>João da Silva</v-row>
+                </v-col>
+                <v-col cols="6" class="mt-5">
+                    <v-row class="mb-2 bold" no-gutters>Início:</v-row>
+                    <v-row no-gutters>10/12/2000 - 11:40</v-row>
+                </v-col>
+                <v-col cols="6" class="mt-5">
+                    <v-row class="mb-2 bold" no-gutters>Fim:</v-row>
+                    <v-row no-gutters>10/12/2000 - 11:50</v-row>
+                </v-col>
+            </v-row>
+        </v-card-text>
+    </v-card>
+</template>
+
+<script lang="ts">
+export default {
+    name: 'event-preview-card',
+    created(){
+    },
+    methods:{
+        
+    }
+}
+</script>
