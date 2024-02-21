de9f73bc24cb2fae2f7091213f18a6af5e4ab14b
mclaramarinho <marinho.claramb@gmail.com>
2024-02-20 21:52:59 -0300

    refactor: removed the event creation form to its own component

--- src/views/private/DashBoard.vue
+++ src/views/private/DashBoard.vue
@@ -1,33 +1,33 @@
 <template>
     <v-container class="h-100 font-blue">
-        <v-row no-gutters class="h-100">
-            <v-col cols="12">
-                <h1 class="">{{ title as string }}</h1>
+        <v-row no-gutters class="h-100" align-content="start">
+            <v-col cols="12" class="mb-5">
+                <v-col cols="12">
+                    <h1 class="">{{ title as string }}</h1>
+                </v-col>
+                <v-col cols="12">
+                    <v-tabs v-if="tabs?.length >= 1" >
+                        <v-tab @click="e => console.log(e.target.value)" :value="item" v-for="item in tabs" :key="item">{{ item }}</v-tab>
+                    </v-tabs>
+                    <v-tabs v-else>
+                        <v-tab>{{ title as string }}</v-tab>
+                    </v-tabs>
+                </v-col>
             </v-col>
+          
             <v-col cols="12">
-                <v-tabs v-if="tabs?.length >= 1" >
-                    <v-tab v-for="item in tabs" :key="item">{{ item }}</v-tab>
-                </v-tabs>
-                <v-tabs v-else>
-                    <v-tab>{{ title as string }}</v-tab>
-                </v-tabs>
-            </v-col>
-            <v-col cols="12">
-                <v-row no-gutters class="mb-4">
-                    <h3 class="">Novo evento</h3>
-                </v-row>
-                <v-row no-gutters>
-                    <v-col cols="12" sm="6" md="4">
-                        <v-form>
-                            <v-text-field type="text" label="Nome" variant="outlined"></v-text-field>
-                            <v-textarea label="Descrição" variant="outlined"></v-textarea>
-                            <v-row no-gutters class="" justify="space-between">
-                                <v-text-field type="datetime-local" label="Início"></v-text-field>
-                                <v-text-field type="datetime-local" label="Fim"></v-text-field>
-                            </v-row>
-                        </v-form>
-                    </v-col>
-                </v-row>
+                <v-container fluid>
+                    <v-row no-gutters class="mb-4">
+                        <h3 class="">Novo evento</h3>
+                    </v-row>
+                    <v-row no-gutters>
+                        <v-col cols="12">
+                            <new-event-form :title="title" />
+                        </v-col>
+                    </v-row>
+                </v-container>
+                
+                
                 
             </v-col>
         </v-row>
@@ -35,8 +35,12 @@
 </template>
 
 <script lang="ts">
+import NewEventForm from './NewEventForm.vue';
 export default {
     name: 'dash-board',
+    components: {
+        NewEventForm
+    },
     props: {
         title: String,
         tabs: {type: Array<string>, default: [] as Array<string>}
