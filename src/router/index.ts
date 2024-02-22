b2cb295cb3d0da21debfede5814c6055d4b720e5
mclaramarinho <marinho.claramb@gmail.com>
2024-02-21 23:03:17 -0300

    feat: dynamic route guarding

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,9 +2,12 @@ import { createRouter, createWebHistory } from 'vue-router'
 import PublicView from '../views/PublicView.vue'
 import { isLoggedIn } from '@/firebase/authorization'
 import { useNavigationHistory } from '@/stores/useNavigationHistory'
+import { useUserInfoStore } from '@/stores/userInfo'
+import type { IUser } from '@/interfaces/IUser'
+import type { User } from 'firebase/auth'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(),
   routes: [
     {
       path: '/',
@@ -34,72 +37,123 @@ const router = createRouter({
     {
       path: '/admin/login',
       name: 'admin-login',
-      beforeEnter: (to, from, next) => {
-        isLoggedIn().then((loggedIn) => {
-          if(loggedIn){
-            useNavigationHistory().setPreviousRoute('/admin/login');
-            next({name: 'retrieving-user-information'})
-          }
-        }).catch((error) => {
-          next()
-        })
-      },
+      meta: {requiresAuth: false, avoid: true},
       component: () => import('../views/AdminLogin.vue')
     },
     {
       path: '/admin/create',
       name: 'admin-create',
+      meta: {requiresAuth: false, avoid: true},
       component: () => import('../views/AdminCreate.vue')
     },
     {
       path: '/admin/retrieving-user-information',
       name: 'retrieving-user-information',
+      meta: {requiresAuth: false},
+      
       component: () => import('../views/private/LoadingUserContent.vue'),
     },
     {
       path: '/admin/dashboard/:id',
       name: 'admin-dashboard',
-      beforeEnter: (to, from, next) => {
-        isLoggedIn().then((loggedIn) => {
-          if(loggedIn){
-            next()
-          }else{
-            next({name: 'retrieving-user-information'})
-          }
-        }).catch((error) => {
-          next({name: 'admin-login'})
-        })
-      },
+      meta: {requiresAuth: true},
       component: () => import('../views/private/AdminDashboard.vue'),
       children: [
         {
           path: '/admin/dashboard/:id/eventos',
-          name: 'admin-calendar',
+          name: 'admin-events',
+          meta: {requiresAuth: true},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/disciplinas',
-          name: 'admin-class-schedule',
+          name: 'admin-classes',
+          meta: {requiresAuth: true},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/contatos',
           name: 'admin-important-contacts',
+          meta: {requiresAuth: true},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/faq',
           name: 'admin-faq',
+          meta: {requiresAuth: true},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/configuracoes',
           name: 'admin-settings',
+          meta: {requiresAuth: true},
           component: () => import('../views/private/AdminDashboard.vue')
         }
       ]
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  console.log("hahaa");
+
+  if(to.meta.avoid){
+    isLoggedIn().then(res => {
+      if(res){
+        next({name: 'retrieving-user-information', query: {redirect: 'admin-dashboard'}})
+      }
+    }).catch(err => {
+      next() 
+    })
+  }else{
+
+    let nextPath = "";
+    if(to.meta.requiresAuth){
+      console.log("hehe");
+
+      isLoggedIn().then((loggedIn) => {
+        if(loggedIn){
+          console.log("loggedIn");
+          
+          //an user is logged in but another is stored
+          if(useUserInfoStore().UID !== (loggedIn as User).uid){
+            nextPath = '/admin/login'
+          }
+          //user is logged in and the same is stored
+          else{
+            nextPath = '/admin/retrieving-user-information'
+          }
+          console.log("nextPath", nextPath);
+          console.log("from", from.path);
+          
+          if(from.path !== '/admin/retrieving-user-information'){
+            
+            if(nextPath === '/admin/retrieving-user-information'){
+              next({path: nextPath, query: {redirect: to.name}})
+            }else{
+              next({path: nextPath})
+            }
+          }else{
+            next()
+          }
+        }
+      }).catch(err => {
+        nextPath = '/admin/login'
+        console.log("err");
+        
+        if(to.meta.requiresAuth){
+          console.log("requiresAuth");
+          next({path: nextPath})
+        }
+      })
+    }else{
+      next()
+    }
+  }
+  
+})
+
+
+
 
 export default router
