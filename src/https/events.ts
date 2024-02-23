d15e1d234de7e162397b491042636c269311384e
mclaramarinho <marinho.claramb@gmail.com>
2024-02-23 11:12:13 -0300

    fix: request body for create event

--- src/https/events.ts
+++ src/https/events.ts
@@ -1,6 +1,7 @@
 import { useUserInfoStore } from "@/stores/userInfo";
 import { BASE_URL, headers } from "./setup";
 
+
 interface IEvent {
     eventName: string;
     isMandatory: boolean;
@@ -9,15 +10,18 @@ interface IEvent {
     eventEnd: string;
     organizer: string | null;
     contact: string | null;
+    location: string | null;
     contactType: "phone" | "email";
 }
 
 interface IHTTPResponse {
+    data: RequestInit;
     code: number;
     response: any;
 }
 
 export async function createEvent(data : IEvent) : Promise<IHTTPResponse>{
+    const res = {} as IHTTPResponse;
     try{
         const serviceURL = `${BASE_URL}/events/create`;
         const body = {
@@ -26,27 +30,32 @@ export async function createEvent(data : IEvent) : Promise<IHTTPResponse>{
             event_name: data.eventName,
             description: data.eventDescription,
             organizer: data.organizer,
+            event_location: data.location,
             event_contact_main: data.contact,
-            is_mandatory: data.isMandatory,
+            is_mandatory: data.isMandatory.toString(),
             created_by: useUserInfoStore().UID,
+            last_edited_by: useUserInfoStore().UID,
         }
+        
         const options = {
             method: 'POST',
             body: JSON.stringify(body),
             headers: headers,
         } as RequestInit;
+        
+        res.data = options;
+        
         const req = await fetch(serviceURL, options);
         const r = await req.json();
-        const res = {
-            code: req.status,
-            response: r,
-        }
+        res.code = req.status;
+        res.response = r;
         return res;
     }catch(err){
-        return {
-            code: 400,
-            response: err
-        }
+        res.code = 400;
+        res.response = err;
+        return res
     }
     
 }
+
+export type {IEvent}
