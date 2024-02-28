f042ec78452bd585d6ffc255bd8caac2d91e7642
mclaramarinho <marinho.claramb@gmail.com>
2024-02-28 17:24:22 -0300

    feat: Refactor authentication logic in router configuration

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import PublicView from '../views/PublicView.vue'
 import { isLoggedIn } from '@/firebase/authorization'
-import { useNavigationHistory } from '@/stores/useNavigationHistory'
 import { useUserInfoStore } from '@/stores/userInfo'
-import type { IUser } from '@/interfaces/IUser'
 import type { User } from 'firebase/auth'
 
 const router = createRouter({
@@ -37,13 +35,33 @@ const router = createRouter({
     {
       path: '/admin/login',
       name: 'admin-login',
-      meta: {requiresAuth: false, avoid: true},
+      meta: {requiresAuth: false},
+      beforeEnter: (to, from, next) => {
+        isLoggedIn().then(res => {
+          if(res){
+            next({name: 'retrieving-user-information', query: {redirect: 'admin-dashboard'}})
+            return;
+          }
+        }).catch(err => {
+          next() 
+        })
+      },
       component: () => import('../views/AdminLogin.vue')
     },
     {
       path: '/admin/create',
       name: 'admin-create',
-      meta: {requiresAuth: false, avoid: true},
+      meta: {requiresAuth: false},
+      beforeEnter: (to, from, next) => {
+        isLoggedIn().then(res => {
+          if(res){
+            next({name: 'retrieving-user-information', query: {redirect: 'admin-dashboard'}})
+            return;
+          }
+        }).catch(err => {
+          next() 
+        })
+      },
       component: () => import('../views/AdminCreate.vue')
     },
     {
@@ -56,37 +74,37 @@ const router = createRouter({
     {
       path: '/admin/dashboard/:id',
       name: 'admin-dashboard',
-      meta: {requiresAuth: true},
+      meta: {requiresAuth: true, avoid: false},
       component: () => import('../views/private/AdminDashboard.vue'),
       children: [
         {
           path: '/admin/dashboard/:id/eventos',
           name: 'admin-events',
-          meta: {requiresAuth: true},
+          meta: {requiresAuth: true, avoid: false},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/disciplinas',
           name: 'admin-classes',
-          meta: {requiresAuth: true},
+          meta: {requiresAuth: true, avoid: false},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/contatos',
           name: 'admin-important-contacts',
-          meta: {requiresAuth: true},
+          meta: {requiresAuth: true, avoid: false},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/faq',
           name: 'admin-faq',
-          meta: {requiresAuth: true},
+          meta: {requiresAuth: true, avoid: false},
           component: () => import('../views/private/AdminDashboard.vue')
         },
         {
           path: '/admin/dashboard/:id/configuracoes',
           name: 'admin-settings',
-          meta: {requiresAuth: true},
+          meta: {requiresAuth: true, avoid: false},
           component: () => import('../views/private/AdminDashboard.vue')
         }
       ]
@@ -95,60 +113,20 @@ const router = createRouter({
 })
 
 router.beforeEach((to, from, next) => {
-  console.log("hahaa");
 
-  if(to.meta.avoid){
-    isLoggedIn().then(res => {
-      if(res){
-        next({name: 'retrieving-user-information', query: {redirect: 'admin-dashboard'}})
-      }
-    }).catch(err => {
-      next() 
-    })
-  }else{
+  if(to.meta.requiresAuth){
+    console.log(to.name);
+    console.log(from.name);
 
-    let nextPath = "";
-    if(to.meta.requiresAuth){
-      console.log("hehe");
-
-      isLoggedIn().then((loggedIn) => {
-        if(loggedIn){
-          console.log("loggedIn");
-          
-          //an user is logged in but another is stored
-          if(useUserInfoStore().UID !== (loggedIn as User).uid){
-            nextPath = '/admin/login'
-          }
-          //user is logged in and the same is stored
-          else{
-            nextPath = '/admin/retrieving-user-information'
-          }
-          console.log("nextPath", nextPath);
-          console.log("from", from.path);
-          
-          if(from.path !== '/admin/retrieving-user-information'){
-            
-            if(nextPath === '/admin/retrieving-user-information'){
-              next({path: nextPath, query: {redirect: to.name}})
-            }else{
-              next({path: nextPath})
-            }
-          }else{
-            next()
-          }
-        }
-      }).catch(err => {
-        nextPath = '/admin/login'
-        console.log("err");
-        
-        if(to.meta.requiresAuth){
-          console.log("requiresAuth");
-          next({path: nextPath})
-        }
-      })
+    if(from.name === undefined){
+      next({path: '/admin/retrieving-user-information', query: {redirect: to.name}})
+    }else if(from.name !== 'retrieving-user-information'){
+      next({path: '/admin/retrieving-user-information', query: {redirect: to.name}})
     }else{
       next()
     }
+  }else{
+      next()
   }
   
 })
