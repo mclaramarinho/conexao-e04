4be1abf73291c3900e6a0591d26ab7b2aeaeb62f
mclaramarinho <marinho.claramb@gmail.com>
2024-03-02 15:54:47 -0300

    interface: IClassesView

---
+++ src/interfaces/IClassesView.ts
@@ -0,0 +1,12 @@
+export default interface IClassesView{
+    id: string;
+    name: string;
+    teacher: string;
+    schedule: string[];
+    classroom: string;
+    exam1Date: string;
+    exam2Date: string;
+    retakeExamDate: string;
+    finalExamDate: string;
+    observations: string;
+}
