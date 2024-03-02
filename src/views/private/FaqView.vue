ac50fb60ce5ad01c2c1adb6b35220bc0bf19786b
mclaramarinho <marinho.claramb@gmail.com>
2024-03-02 15:58:10 -0300

    feat: added the add dialog

--- src/views/private/FaqView.vue
+++ src/views/private/FaqView.vue
@@ -1,17 +1,22 @@
 <template>
   <multiuse-data-table :headers="headers" :table-items="faqs" item-value="question"
-              :add-new-button="true" 
               expandable expandable-item-key="answer"
               show-expandable-item-actions :expandable-item-actions="tableActions"
+              @update="fetchItems()" :loading="loading"
               @edit="e => editItem(e)" @delete="e => deleteItem(e)"
   >
     <template v-slot:addDialog>
-        <faq-add-edit variant="add" />
+        <v-dialog v-model="dialogAdd" max-width="500px">
+            <template v-slot:activator>
+                <v-btn color="var(--dark-blue)" v-model="dialogAdd" @click="dialogAdd = true" dark class="">Criar Nova</v-btn>
+            </template>
+            <faq-add-edit variant="add" @cancel="dialogAdd = false" @done="dialogAdd = false" />
+        </v-dialog>
     </template>
   </multiuse-data-table>
 
   <v-dialog v-model="dialogEdit" max-width="500px">
-    <faq-add-edit variant="edit" :item="selectedItem" @cancel="dialogEdit = false" />
+    <faq-add-edit variant="edit" :item="selectedItem" @cancel="dialogEdit = false" @done="dialogEdit=false" />
   </v-dialog>
   <v-dialog v-model="dialogDelete" max-width="500px">
     <dialog-delete-item />
@@ -22,6 +27,8 @@
 import DialogDeleteItem from '@/components/smaller_components/dialogs/DialogDeleteItem.vue';
 import FaqAddEdit from '@/components/smaller_components/dialogs/FaqAddEdit.vue'
 import MultiuseDataTable from '@/components/MultiuseDataTable.vue'
+import {getAllFAQs} from '@/https/faqs'
+
 export default {
     name: 'faq-view',
     components: {
@@ -30,6 +37,7 @@ export default {
   data: () => ({
     dialogAdd: false,
     dialogDelete: false,
+    loading: false,
     dialogEdit: false,
     selectedItem: {} as {id: string, question: string, answer: string},
     headers: [
@@ -40,125 +48,35 @@ export default {
         key: 'question'
       },
     ],
-    faqs: [
-        {
-            id: "123423",
-            question: "What is the capital of France?",
-            answer: "Paris"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of the United States?",
-            answer: "Joe Biden"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Peru?",
-            answer: "Lima"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Brazil?",
-            answer: "Jair Bolsonaro. Unfortunately."
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Argentina?",
-            answer: "Buenos Aires"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Argentina?",
-            answer: "Alberto Fernández"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Chile?",
-            answer: "Santiago"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Chile?",
-            answer: "Sebastián Piñera"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Colombia?",
-            answer: "Bogotá"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Colombia?",
-            answer: "Iván Duque Márquez"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Venezuela?",
-            answer: "Caracas"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Venezuela?",
-            answer: "Nicolás Maduro"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Ecuador?",
-            answer: "Quito"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Ecuador?",
-            answer: "Lenín Moreno"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Bolivia?",
-            answer: "Sucre"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Bolivia?",
-            answer: "Luis Arce"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Paraguay?",
-            answer: "Asunción"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Paraguay?",
-            answer: "Mario Abdo Benítez"
-        },
-        {
-            id: "123423",
-            question: "What is the capital of Uruguay?",
-            answer: "Montevideo"
-        },
-        {
-            id: "123423",
-            question: "Who is the president of Uruguay?",
-            answer: "Luis Lacalle Pou"
-        }
-    ] as {id: string, question: string, answer: string}[],
+    faqs: [] as Array<Object>,
     tableActions:[
       {name: 'Excluir', color: 'var(--danger-red)', eventName: 'delete', prependIcon:'mdi-delete'},
       {name: 'Editar', color: 'var(--dark-blue)', eventName: 'edit', prependIcon:'mdi-pencil'}
     ]
   }),
   created() {
+    this.fetchItems()
   },
 
   methods: {
+    async fetchItems(){
+        this.loading = true;
+        const res = await getAllFAQs();
+        if(res.code === 200){
+            console.log(res)
+            this.faqs = res.response;
+        }else{
+            console.log(res)
+        }
+        this.loading = false;
+    },
     editItem(item) {
-        console.log(item);
-        this.selectedItem = Object.assign({}, item)
+        this.selectedItem = item
         this.dialogEdit = true;
     },
 
     deleteItem(item) {       
-        this.selectedItem = Object.assign({}, item)
+        this.selectedItem = item
         this.dialogDelete = true
     },
 
@@ -167,18 +85,8 @@ export default {
     },
 
     closeDelete() {
-        
         this.dialogDelete = false
     },
-
-    // save() {
-    //   if (this.editedIndex > -1) {
-    //     Object.assign(this.desserts[this.editedIndex], this.editedItem)
-    //   } else {
-    //     this.desserts.push(this.editedItem)
-    //   }
-    //   this.close()
-    // }
   }
 }
 </script>
