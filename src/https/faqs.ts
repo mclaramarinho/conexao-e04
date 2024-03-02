2074d2f05c93eeb7c6c0ee14c8ab9a1e09567ddd
mclaramarinho <marinho.claramb@gmail.com>
2024-03-02 15:54:34 -0300

    feat: http methods for faq

---
+++ src/https/faqs.ts
@@ -0,0 +1,90 @@
+import { BASE_URL, headers } from '@/https/setup'
+import type { IHTTPResponse } from '@/https/setup'
+import { useUserInfoStore } from '@/stores/userInfo';
+
+export interface IFaq{
+    question: string;
+    answer: string;
+}
+
+export async function createFAQ(data : IFaq) : Promise<IHTTPResponse>{
+    const serviceURL = `${BASE_URL}/faq/create`;
+    const res = {} as IHTTPResponse;
+    const body = {
+        question: data.question,
+        answer: data.answer,
+        created_by: useUserInfoStore().UID,
+        last_edited_by: useUserInfoStore().UID
+    };
+    
+
+    try{
+        const options = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        } as RequestInit;
+        res.data = options;
+
+        const req = await fetch(serviceURL, options);
+        const resBody = await req.json();
+        res.code = req.status;
+        res.response = resBody;
+        return res;
+    }catch(err){
+        res.code = 400;
+        res.response = err;
+        return res;
+    }
+}
+
+export async function updateFAQ(data : IFaq, id : string) : Promise<IHTTPResponse>{
+            
+    const serviceURL = `${BASE_URL}/faq/update/${id}`;
+    const body = {
+        question: data.question,
+        answer: data.answer,
+        last_edited_by: useUserInfoStore().UID
+    };
+    const res = {} as IHTTPResponse;
+
+    try{
+        const options = {
+            method: 'PUT',
+            headers: headers,
+            body: JSON.stringify(body)
+        } as RequestInit;
+        res.data = options;
+        const req = await fetch(serviceURL, options);
+        const resBody = await req.json();
+        res.code = req.status;
+        res.response = resBody;
+        return res;
+    }catch(err){
+        res.code = 400;
+        res.response = err;
+        return res;
+    }
+}
+
+export async function getAllFAQs() : Promise<IHTTPResponse>{
+    const serviceURL = `${BASE_URL}/faq/all`;
+    const res = {} as IHTTPResponse;
+    const options = {
+        method: 'GET',
+        headers: headers
+    } as RequestInit;
+    res.data = options;
+
+    try{
+        const req = await fetch(serviceURL, options);
+        const resBody = await req.json();
+        res.code = req.status;
+        res.response = resBody;
+        return res;
+    }catch(err){
+        res.code = 400;
+        res.response = err;
+        return res;
+    }
+}
