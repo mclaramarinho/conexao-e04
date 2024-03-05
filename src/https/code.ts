ca0e6cd15ab0dddf134c0cefc68c92cd3bd5d8f3
mclaramarinho <marinho.claramb@gmail.com>
2024-03-05 16:30:42 -0300

    feat: get all valid codes

--- src/https/code.ts
+++ src/https/code.ts
@@ -1,6 +1,7 @@
 import {BASE_URL, headers} from './setup';
 import { useAccountCreationStore } from '@/stores/accountCreation';
 import type { IHTTPResponse } from './setup';
+import { useUserInfoStore } from '@/stores/userInfo';
 
 export async function validateCode() : Promise<IHTTPResponse>{
     const res = {} as IHTTPResponse;
@@ -22,8 +23,89 @@ export async function validateCode() : Promise<IHTTPResponse>{
         res.response = response;
         return res;
     }catch(err){
+        // TODO - handle error more specifically
         res.code = 400;
         res.response = err;
         return res
+    }
+}
+
+export async function getValidCodes(isExpired : boolean) : Promise<IHTTPResponse>{
+    const uid = useUserInfoStore().UID;
+    const serviceUrl = `${BASE_URL}/code/all/${uid}/${isExpired}`;
+    const res = {} as IHTTPResponse;
+    const options = {
+        method: 'GET',
+        headers: headers
+    } as RequestInit;
+    res.data = options;
+    try{
+        const req = await fetch(serviceUrl, options);
+        const resBody = await req.json();
+        
+        res.code = req.status;
+        res.response = resBody;
+        
+        return res;
+    }catch(err){
+        // TODO - handle error more specifically
+        res.code = 400;
+        res.response = err;
+        return res;
+    }
+}
+
+export async function createNewCode(role : 'owner' | 'admin') : Promise<IHTTPResponse>{
+    const uid = useUserInfoStore().UID;
+    const body = {
+        uid: uid,
+        role: role
+    };
+    const res = {} as IHTTPResponse;
+    const serviceUrl = `${BASE_URL}/code/create`
+    
+
+    try{
+        const options = {
+            method: 'POST',
+            headers: headers,
+            body: JSON.stringify(body)
+        } as RequestInit;
+        res.data = options;
+        const req = await fetch(serviceUrl, options);
+        const resBody = await req.json();
+        res.code = req.status;
+        res.response = resBody;
+        return res;
+    }catch(err){
+        // TODO - handle error more specifically
+        res.code = 400;
+        res.response = err;
+        return res;
+    }
+}
+
+export async function deleteCode(code : string) : Promise<IHTTPResponse>{
+    const uid = useUserInfoStore().UID;
+    const serviceUrl = `${BASE_URL}/code/delete/${code}/${uid}`;
+    const options = {
+        method: 'DELETE',
+        headers: headers
+    } as RequestInit;
+
+    const res = {} as IHTTPResponse;
+
+    res.data = options;
+
+    try{
+        const req = await fetch(serviceUrl, options);
+        res.code = req.status;
+        res.response = {};
+        return res;
+    }catch(err){
+        // TODO - handle error more specifically
+        res.code = 400;
+        res.response = err;
+        return res;
     }
 }
