be7adc8051a344667cd6c9746ce2b4e7a5084ecf
mclaramarinho <marinho.claramb@gmail.com>
2024-03-05 22:14:45 -0300

    feat: created an interface for http methods + a request options builder

--- src/https/setup.ts
+++ src/https/setup.ts
@@ -9,6 +9,25 @@ export interface IHTTPResponse {
     data: RequestInit;
     code: number;
     response: any;
+};
+
+export interface IHTTPMethods{
+    method: 'GET' | 'POST' | 'PUT' | 'DELETE';
+}
+
+export function createOptions(method : IHTTPMethods, body : any | undefined) : RequestInit{
+    if(body){
+        return{
+            method: method.method,
+            headers: headers,
+            body: JSON.stringify(body)
+        }
+    }else{
+        return{
+            method: method.method,
+            headers: headers
+        }
+    }
 }
 
 // TODO - Create a collection of endpoints
