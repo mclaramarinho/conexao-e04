2ee10a48293c209c9f5d94b9bab4129e15771e12
mclaramarinho <marinho.claramb@gmail.com>
2024-03-06 14:58:08 -0300

    refactor: Update IClassesGetBody to IClassFormatted in ClassesView.vue

--- src/views/private/ClassesView.vue
+++ src/views/private/ClassesView.vue
@@ -53,7 +53,7 @@
 <script lang="ts">
 import { getAllClasses, deleteClass } from '@/https/classes';
 import type { IHTTPResponse } from '@/https/setup';
-import type {IClassesGetBody} from '@/interfaces/Https';
+import type {IClass, IClassFormatted, IClassGetBody} from '@/interfaces/Https';
 import MultiuseDataTable from '@/components/MultiuseDataTable.vue';
 import DialogDeleteItem from '@/components/smaller_components/dialogs/DialogDeleteItem.vue';
 import ClassEdit from '@/components/smaller_components/dialogs/ClassEdit.vue';
@@ -64,7 +64,7 @@ export default {
     },
     data(){
         return {
-            classes: [] as Array<IClassesGetBody>,
+            classes: [] as Array<IClassFormatted>,
             loadingTable: false,
             showDeleteDialog: false,
             showEditDialog: false,
@@ -110,44 +110,50 @@ export default {
         async fetchClasses(){
             this.loadingTable = true;
 
-            const response: IHTTPResponse = await getAllClasses();
+            const response = await getAllClasses();
 
             this.loadingTable = false;
 
             if(response.code === 200){
-                const localClasses = response.response;
+                console.log(response.response);
+                
+                const localClasses = response.response as Array<IClassGetBody | null>;
 
                 this.classes = localClasses.map(item => {
-                    const schedule = item.days.map((day : string, index : number) => {
+                    const schedule = item?.days.map((day : string, index : number) => {
                         const startTime = new Date(item.start_time[index]);
                         const fStart = startTime.getHours().toString().padStart(2, '0') + ':' + startTime.getMinutes().toString().padStart(2, '0');
+
                         const endTime = new Date(item.end_time[index]);
                         const fEnd = endTime.getHours().toString().padStart(2, '0') + ':' + endTime.getMinutes().toString().padStart(2, '0');
 
                         return `${day}: ${fStart} - ${fEnd}`
                     })
                     // TODO - Crete a function to handle the date formatting in a date utils file
-                    const exam1Date = new Date(item.exam_1_timestamp);
-                    const fExam1Date = exam1Date.getDate().toString().padStart(2, '0') + '/' + (exam1Date.getMonth() + 1).toString().padStart(2, '0') + '/' + exam1Date.getFullYear() + ' - ' + exam1Date.getHours().toString().padStart(2, '0') + ':' + exam1Date.getMinutes().toString().padStart(2, '0');
-                    const exam2Date = new Date(item.exam_2_timestamp);
-                    const fExam2Date = exam2Date.getDate().toString().padStart(2, '0') + '/' + (exam2Date.getMonth() + 1).toString().padStart(2, '0') + '/' + exam2Date.getFullYear() + ' - ' + exam2Date.getHours().toString().padStart(2, '0') + ':' + exam2Date.getMinutes().toString().padStart(2, '0');
-                    const retakeExamDate = new Date(item.re_take_exam_timestamp);
-                    const fRetakeExamDate = retakeExamDate.getDate().toString().padStart(2, '0') + '/' + (retakeExamDate.getMonth() + 1).toString().padStart(2, '0') + '/' + retakeExamDate.getFullYear() + ' - ' + retakeExamDate.getHours().toString().padStart(2, '0') + ':' + retakeExamDate.getMinutes().toString().padStart(2, '0');
-                    const finalExam = new Date(item.final_exam_timestamp);
-                    const fFinalExam = finalExam.getDate().toString().padStart(2, '0') + '/' + (finalExam.getMonth() + 1).toString().padStart(2, '0') + '/' + finalExam.getFullYear() + ' - ' + finalExam.getHours().toString().padStart(2, '0') + ':' + finalExam.getMinutes().toString().padStart(2, '0');
+                    const exam1Date = item && new Date(item.exam_1_timestamp);
+                    const fExam1Date = exam1Date && exam1Date.getDate().toString().padStart(2, '0') + '/' + (exam1Date.getMonth() + 1).toString().padStart(2, '0') + '/' + exam1Date.getFullYear() + ' - ' + exam1Date.getHours().toString().padStart(2, '0') + ':' + exam1Date.getMinutes().toString().padStart(2, '0');
+                    const exam2Date = item && new Date(item.exam_2_timestamp);
+                    const fExam2Date = exam2Date && exam2Date.getDate().toString().padStart(2, '0') + '/' + (exam2Date.getMonth() + 1).toString().padStart(2, '0') + '/' + exam2Date.getFullYear() + ' - ' + exam2Date.getHours().toString().padStart(2, '0') + ':' + exam2Date.getMinutes().toString().padStart(2, '0');
+                    const retakeExamDate = item && new Date(item.re_take_exam_timestamp);
+                    const fRetakeExamDate = retakeExamDate && retakeExamDate.getDate().toString().padStart(2, '0') + '/' + (retakeExamDate.getMonth() + 1).toString().padStart(2, '0') + '/' + retakeExamDate.getFullYear() + ' - ' + retakeExamDate.getHours().toString().padStart(2, '0') + ':' + retakeExamDate.getMinutes().toString().padStart(2, '0');
+                    const finalExam = item && new Date(item.final_exam_timestamp);
+                    const fFinalExam = finalExam && finalExam?.getDate().toString().padStart(2, '0') + '/' + (finalExam.getMonth() + 1).toString().padStart(2, '0') + '/' + finalExam.getFullYear() + ' - ' + finalExam.getHours().toString().padStart(2, '0') + ':' + finalExam.getMinutes().toString().padStart(2, '0');
 
                     return {
-                        id: item._id,
-                        name: item.name,
-                        teacher: item.teacher,
+                        id: item?._id,
+                        name: item?.name,
+                        teacher: item?.teacher,
                         schedule: schedule,
-                        classroom: item.classroom,
+                        classroom: item?.classroom,
                         exam1Date: fExam1Date,
                         exam2Date: fExam2Date,
                         retakeExamDate: fRetakeExamDate,
                         finalExamDate: fFinalExam,
-                        observations: item.observations
-                    } as IClassesGetBody;
+                        observations: item?.observations,
+                        days: [],
+                        startTime: '',
+                        endTime: ''
+                    } as IClassFormatted;
                 })
             }else{
                 console.log(response);
