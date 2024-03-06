4e647542f575350f986c155edecbddfc2a15ce2f
mclaramarinho <marinho.claramb@gmail.com>
2024-03-06 13:27:24 -0300

    component: created circular loader

---
+++ src/components/smaller_components/loaders/CircularLoader.vue
@@ -0,0 +1,11 @@
+<template>
+    <v-row no-gutters class="position-relative h-100" align="center">
+        <v-progress-circular indeterminate class="mx-auto font-blue" />
+    </v-row>
+</template>
+
+<script lang="ts">
+export default{
+    name: 'circular-loader'
+}
+</script>
