64e2dae2ba10cbafe89be84dfcb466a8e83d6a58
mclaramarinho <marinho.claramb@gmail.com>
2024-03-06 13:28:41 -0300

    feat: fetching contacts from database + loader component

--- src/components/ImportantContacts.vue
+++ src/components/ImportantContacts.vue
@@ -1,4 +1,5 @@
 <template>
+    <circular-loader v-if="!render" />
     <v-container id="ic_container" class="h-100">
         <v-row no-gutters class="">
             <v-col cols="10" sm="6" class="mx-auto">
@@ -6,8 +7,8 @@
             </v-col>
         </v-row>
         <v-row no-gutters>
-            <v-col cols="12" sm="9" md="10" class="mx-auto">
-                <contact-card v-for="c in filteredResult" :key="c.name" :name="c.name" :phone="c.phone" :email="c.email" />
+            <v-col v-if="filteredResult.length > 0" cols="12" sm="9" md="10" class="mx-auto">
+                <contact-card v-for="c in filteredResult" :key="c.name" :name="c.name" :phone="c.phone_number" :email="c.email" />
             </v-col>
         </v-row>
     </v-container>
@@ -16,25 +17,55 @@
 <script lang="ts">
 import SearchBar from './smaller_components/text-fields/SearchBar.vue';
 import ContactCard from './smaller_components/cards/ContactCard.vue';
-import { contacts } from '@/stores/temp/contacts';
+import type { IContactGetBody } from '@/interfaces/Https';
+import { getContacts } from '@/https/contacts';
+import CircularLoader from './smaller_components/loaders/CircularLoader.vue';
 export default {
     name: 'ImportantContacts',
     components: {
-        SearchBar, ContactCard
+        SearchBar, ContactCard, CircularLoader
     },
     data() {
         return {
-            // TODO - replace with actual data brought from the api 
-            filteredResult: [] as Array<{name: string, phone: string, email: string}>,
-            showScrollTopBtn: false
+            contacts: [] as Array<IContactGetBody>,
+            filteredResult: [] as Array<IContactGetBody>,
+            showScrollTopBtn: false,
+            render: false
         }
     },
-    mounted(){
-        this.filteredResult = contacts
+    async created(){
+        await this.fetchContacts()
     },
     methods:{
         filterContacts(v : string){
-            this.filteredResult = contacts.filter(item => item.name.toLowerCase().includes(v.toLowerCase()) || item.phone.includes(v) || item.email.toLowerCase().includes(v.toLowerCase()))
+            this.filteredResult = this.contacts.filter(item => {
+                const fName = item.name.toLowerCase();
+                const fPhone = item.phone_number.toLowerCase();
+                const fEmail = item.email.toLowerCase();
+                const fValue = v.toLowerCase();
+                if(fName.includes(fValue) || fPhone.includes(fValue) || fEmail.includes(fValue)){
+                    return item;
+                }
+            })
+        },
+        async fetchContacts(){
+            this.render = false;
+            try{
+                const res = await getContacts();
+                if(res.code === 200){
+                    this.contacts = res.response;
+                }else{
+                    throw new Error('Failed to fetch contacts');
+                }
+            }catch(err){
+                console.error(err);
+                //TODO - show error message
+                this.contacts = [];
+            }
+
+            this.filteredResult = this.contacts;
+
+            this.render = true;
         }
     }
 }
