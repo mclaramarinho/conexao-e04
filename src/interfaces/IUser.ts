d458bb29de8b5c12dd153eed25028671f36b92ff
mclaramarinho <marinho.claramb@gmail.com>
2024-03-05 22:15:58 -0300

    refactor: deleted this interface to the Https interface file

--- src/interfaces/IUser.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser{
-    firebase_uid: String | null,
-    name: String | null,
-    email: String | null,
-    role: String | null,
-    last_login: String | null,
-    creation_date_timestamp: String | null
-}
