693cf648b1646ca6805747215c92f079cffd6ed7
mclaramarinho <marinho.claramb@gmail.com>
2024-03-11 09:44:37 -0300

    fix: text center + dialog to show details

--- src/components/ClassSchedule.vue
+++ src/components/ClassSchedule.vue
@@ -8,26 +8,27 @@
                 <th class="text-center">FIM</th>
                 <th class="text-center">DISCIPLINA</th>
                 <th class="text-center">SALA</th>
+                <th class="text-center"></th>
             </tr>
         </thead>
 
         <tbody>
-            <tr class="text-center" v-for="(day, i) in weekdays" :key="i">
-                <div v-if="day.classes.length > 1">
+            <tr v-for="(day, i) in weekdays" :key="i">
+                <div v-if="day.classes.length > 1" class='mx-auto h-100'>
                     <br v-for="(j) in day.classes.length" :key="j">
-                        {{ day.abbr.toUpperCase() }}
+                       <p class="text-center">{{ day.abbr.toUpperCase() }}</p> 
                     <br v-for="(j) in day.classes.length" :key="j">
                 </div>
-                <div v-else>
+                <div v-else class='mx-auto h-100'>
                     <br>
-                    {{ day.abbr.toUpperCase() }}
+                    <p class="text-center">{{ day.abbr.toUpperCase() }}</p> 
                     <br>
                 </div>
                 <!-- START -->
                 <td>
                     <br v-if="day.classes.length > 1">
                     <div v-for="(c, index) in day.classes" :key="index">
-                        <tr>{{ getTime(c.start) }}</tr>
+                        <p class="text-center">{{ c.start === null ? '-' : getTime(c.start) }}</p>
                         <br v-if="day.classes.length > 1">
                     </div>
                 </td>
@@ -35,7 +36,7 @@
                 <td>
                     <br v-if="day.classes.length > 1">
                     <div v-for="(c, index) in day.classes" :key="index">
-                        <tr>{{ getTime(c.end) }}</tr>
+                        <p class="text-center">{{ c.end === null ? '-' : getTime(c.end) }}</p>
                         <br v-if="day.classes.length > 1">
                     </div>
                 </td>
@@ -43,15 +44,24 @@
                 <td>
                     <br v-if="day.classes.length > 1">
                     <div v-for="(c, index) in day.classes" :key="index">
-                        <tr>{{ c.name }}</tr>
+                        <p class="text-center">{{ c.name }}</p>
                         <br v-if="day.classes.length > 1">
                     </div>
                 </td>
                 <!-- ROOM -->
                 <td>
                     <br v-if="day.classes.length > 1">
-                    <div v-for="(c, index) in day.classes" :key="index" class="justify-content-center text-center">
-                        <tr>{{ c.room }}</tr>
+                    <div v-for="(c, index) in day.classes" :key="index">
+                        <p class="text-center">{{ c.room }}</p>
+                        <br v-if="day.classes.length > 1">
+                    </div>
+                </td>
+                <!-- MORE INFO -->
+                <td>
+                    <br v-if="day.classes.length > 1">
+                    <div v-for="(c, index) in day.classes" :key="index">
+                        <p class="text-center"><v-btn icon="mdi-plus" size="xs" variant="text" color="var(--dark-blue)"
+                            @click="handleOpenDetails(c._id)"/></p>
                         <br v-if="day.classes.length > 1">
                     </div>
                 </td>
@@ -59,16 +69,20 @@
 
         </tbody>
     </v-table>
+    <v-dialog max-width="500px" :model-value="details.open" @update:model-value="details.open = false">
+        <class-edit :id="details.classId" :allowEditing="false" />
+    </v-dialog>
 </template>
 
 <script lang="ts">
 import type { IClassGetBody } from '@/interfaces/Https';
 import { getAllClasses } from '@/https/classes';
 import CircularLoader from './smaller_components/loaders/CircularLoader.vue';
+import ClassEdit from './smaller_components/dialogs/ClassEdit.vue';
 export default {
     name: 'ClassSchedule',
     components: {
-        CircularLoader
+        CircularLoader, ClassEdit
     },
     data() {
         return {
@@ -103,8 +117,12 @@ export default {
                 saturday: {
                     name: 'Sábado',
                     abbr: 'Sáb',
-                    classes: [] as Array<Object>
+                    classes: [] as Array<IClassGetBody>
                 }
+            },
+            details:{
+                open: false as boolean,
+                classId: '' as string,
             },
             render: false
         }
@@ -121,9 +139,27 @@ export default {
                     this.classes = res.response;
                     this.classes.map(c => {
                         const days = c.days;
-
+                        if(days.length === 0){
+                            this.weekdays['saturday'].classes.push({
+                                _id: c._id,
+                                name: c.name,
+                                start: null,
+                                end: null,
+                                room: c.classroom,
+                                teacher: c.teacher,
+                                exam1: c.exam_1_timestamp,
+                                exam2: c.exam_2_timestamp,
+                                retake: c.re_take_exam_timestamp,
+                                final: c.final_exam_timestamp
+                            })
+                        }
                         days.map((d, index) => {
-                            const en = d === 'Segunda-Feira' ? 'monday' : d === 'Terça-Feira' ? 'tuesday' : d === 'Quarta-Feira' ? 'wednesday' : d === 'Quinta-Feira' ? 'thursday' : d === 'Sexta-Feira' ? 'friday' : 'saturday';
+                            let en = d === 'Segunda-Feira' ? 'monday' 
+                                        : d === 'Terça-Feira' ? 'tuesday' 
+                                        : d === 'Quarta-Feira' ? 'wednesday' 
+                                        : d === 'Quinta-Feira' ? 'thursday' 
+                                        : d === 'Sexta-Feira' ? 'friday' 
+                                        : 'saturday';
                             this.weekdays[en].classes.push({
                                 _id: c._id,
                                 name: c.name,
@@ -143,7 +179,6 @@ export default {
                 }
             }catch(err){
                 console.error(err);
-                // TODO - show error message
                 this.classes = [];
             }
             this.render = true;
@@ -153,6 +188,10 @@ export default {
             const hours = date.getHours().toString().padStart(2, '0');
             const minutes = date.getMinutes().toString().padStart(2, '0');
             return `${hours}:${minutes}`;
+        },
+        handleOpenDetails(id : string){
+            this.details.classId = id;
+            this.details.open = true;
         }
     }
 }
